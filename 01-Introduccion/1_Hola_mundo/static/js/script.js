89cedd2efff1a5b05fd6c388dea35d17415e4a9b
AATPVVC <angelalvarez@liceovvh.cl>
2026-04-10 08:19:22 -0400

    a

---
+++ 01-Introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+//Prueba de conexión con js
+console.log("Hola mundo,desde el archivo externo!...");
+
+//1) Hola mundo con variables y cont.
+const Saludo = "Hola a todos,";
+let destinatario = " estudiantes";
+//
+console.log(Saludo + "para los " + destinatario);
+
+// 2) Saludar 
+let nombre = prompt("Ingrese su nombre ");
+//Inprimir un nombre ingresado con un prompt
+console.log("Hola, " + nombre);
---
+++ 02_tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta...")
+/*
+Dato: tipo texto(string)
+Concatenacion de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = " Angel";
+const apellido = "Alvarez";
+
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
