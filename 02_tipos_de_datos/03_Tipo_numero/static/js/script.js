5e33cc2f07198b2a7463667d72f26e6ae62c6384
AATPVVC <angelalvarez@liceovvh.cl>
2026-04-23 12:00:41 -0400

    ok

---
+++ 02_tipos_de_datos/03_Tipo_numero/static/js/script.js
@@ -0,0 +1,12 @@
+//8.- NUmeros aleatorios
+let aleatotio = Math.random();
+let dado = Math.floor(Math.random()*6) +1
+console.log(`Tirar dado: ${dado}`);
+
+//9.- Comparaciones numericas
+let a = 10;
+let b = 5;
+console.log(a > b); //true
+console.log(a < b); // false
+console.log(a === 10); //true
+console.log(b === 3); //false
