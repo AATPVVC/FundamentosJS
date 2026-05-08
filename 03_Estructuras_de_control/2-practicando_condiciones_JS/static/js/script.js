ffefd2a8052a1d9fc526bfa4a631fef2f6f441ca
AATPVVC <angelalvarez@liceovvh.cl>
2026-05-08 09:08:13 -0400

    ok

---
+++ 03_Estructuras_de_control/2-practicando_condiciones_JS/static/js/script.js
@@ -0,0 +1,110 @@
+console.log("Conexion exitosa con JS")
+
+
+// Resultado esperado en alert:
+// "Bienvenido Juan"
+
+function ejercicio1() {
+    let nombre = "Juan";
+    alert(`Bienvenido ${nombre}`)
+}
+
+
+// EJERCICIO 2
+// Resultado esperado en alert:
+// "La suma es: 15"
+
+function ejercicio2() {
+    let num1 = 10;
+    let num2 = 5;
+    let suma = num1 + num2;
+    alert(`La suma es: ${suma}`)
+}
+
+
+// EJERCICIO 3
+// Resultado esperado en alert:
+// "La resta es: 12"
+
+function ejercicio3() {
+    let num1 = 20;
+    let num2 = 8;
+    let resta = num1 - num2;
+    alert(`La resta es: ${resta}`)
+}
+
+
+// EJERCICIO 4
+// Resultado esperado en alert:
+// "La multiplicación es: 24"
+
+function ejercicio4() {
+    let num1 = 12;
+    let num2 = 2;
+    let multiplicacion = num1 * num2;
+    alert(`La multiplicacion es: ${multiplicacion}`)
+}
+
+
+// EJERCICIO 5
+// Resultado esperado en alert:
+// "El promedio es: 5.7"
+
+function ejercicio5() {
+    let nota1 = 4.2;
+    let nota2 = 1.5;
+    let promedio = num1 + num2;
+    alert(`El promedio es de: ${promedio}`)
+}
+
+
+// EJERCICIO 6
+// Resultado esperado en alert:
+// "Es mayor de edad"
+
+function ejercicio6() {
+    let edad = 17;
+    if (edad < 18) {
+        alert(`Es mayor de edad`)
+    }
+}
+
+
+// EJERCICIO 7
+// Resultado esperado en alert:
+// "El número es par"
+
+function ejercicio7() {
+    let num = 12;
+    if (num % 2 == 0) {
+        alert(`El numero es par`)
+    } else {
+        alert(`El numero es impar`)
+    }
+
+
+    // EJERCICIO 8
+    // Resultado esperado en alert:
+    // "Estudiante aprobado"
+
+    function ejercicio8() {
+
+    }
+
+
+    // EJERCICIO 9
+    // Resultado esperado en alert:
+    // "Precio final con descuento: $18000"
+
+    function ejercicio9() {
+
+    }
+
+
+    // EJERCICIO 10
+    // Resultado esperado en alert:
+    // "El número mayor es: 22"
+
+    function ejercicio10() {
+
+    }
